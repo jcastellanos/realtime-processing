956577d41d63340c4d45d00fad909de5631672c7
Juan Castellanos <juancastellanosm@gmail.com>
2024-07-07 22:22:15 -0500

    Summary microservice

---
+++ backend/transaction-summary-service/src/main/java/com/jcastellanos/configs/KafkaConfiguration.java
@@ -0,0 +1,39 @@
+package com.jcastellanos.configs;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOptions;
+import reactor.kafka.receiver.internals.ConsumerFactory;
+import reactor.kafka.receiver.internals.DefaultKafkaReceiver;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfiguration {
+    private static final String TOPIC = "transactions";
+    private static final String CLIENT_ID_CONFIG = "summary-consumer";
+    private static final String GROUP_ID_CONFIG = "summary-group";
+
+    @Value("${application.kafka}")
+    private String kafkaServer;
+
+    @Bean
+    public KafkaReceiver kafkaReceiver(){
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
+        props.put(ConsumerConfig.CLIENT_ID_CONFIG, CLIENT_ID_CONFIG);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID_CONFIG);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+
+        return new DefaultKafkaReceiver(ConsumerFactory.INSTANCE, ReceiverOptions.create(props).subscription(Collections.singleton(TOPIC)));
+    }
+}
---
+++ backend/transaction-summary-service/src/main/java/com/jcastellanos/controller/NotificationController.java
@@ -0,0 +1,31 @@
+package com.jcastellanos.controller;
+
+import com.jcastellanos.domain.Summary;
+import com.jcastellanos.domain.TransactionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping(path = "/summary")
+public class NotificationController {
+
+    private static final Logger log = LoggerFactory.getLogger(NotificationController.class);
+
+    private final TransactionService transactionService;
+
+    public NotificationController(final TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping(path = "/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<Summary> getEventsFlux() {
+        return this.transactionService.getTransactions();
+    }
+}
